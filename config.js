35937f752a0acf8c9ba2b2e1fb982080678d51ef
akiyabota <112392909+akiyabota@users.noreply.github.com>
2023-06-08 20:15:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split(",")
+global.owner = process.env.OWNER_NUMBER.split("785918674")
 global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
 global.port= process.env.PORT || 5000
 global.email = 'shashikachirath1@gmail.com'
@@ -16,15 +16,15 @@ global.website = 'https://github.com/mrhansamala/-Queen-Nethu-MD-Bot-' //wa.me/+
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/d229929fd0e975d64010c.jpg'
 module.exports = {
   botname: process.env.BOT_NAME || 'Queen Nethu',
-  ownername:process.env.OWNER_NAME || 'MR.Hansamala',
-  sessionName: process.env.SESSION_ID || 'PUT-HERE',
+  ownername:process.env.OWNER_NAME || 'MR.Akila',
+  sessionName: process.env.SESSION_ID || 'eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTUhJQUd4MlJVL3FJcy9Fd25NRGZkV09iaGJRQW5WWWt1UmtwQlNWR09HOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTURKdWRtb3ZuZDV6WnAzdy80QzJwOGRnQkdtckpLWUNDRzFjS251MjRIWT0ifX0sInNpZ25lZElkZW50aXR5S2V5Ijp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJhSGo5bHBiQ01iVHMzc295cHNEaWJyVmU5ODZJZnFGR3dWdUlxZkk0dVc4PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJVTEFjTlV6YnBQUW1YdFlpMWVzMUNsc084SldBWlRjVU9wMi9Vdm8xOW1ZPSJ9fSwic2lnbmVkUHJlS2V5Ijp7ImtleVBhaXIiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InlBbG5saDdtcHZLdG1qdEhJRnlnYmhrNkdwOG1lN3o3WFArRkdqUGU3MGc9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Imtrd0h4ekhieG53RHRJcnZnYkc4WTRabkFYaUhKSjVhcVFLTCtnL1IxbWc9In19LCJzaWduYXR1cmUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJrK1l4RXdiemMwZUZUY1VsMHM3QWZPd2xsQXR2ZmQwZlhvalRtTE9kUitLMEN1Zm1aSkcvZjR5dmJYMEptSXJFZXpSeWlpV1VNSmFnMzdNUEZ2amNpUT09In0sImtleUlkIjoxfSwicmVnaXN0cmF0aW9uSWQiOjI0NywiYWR2U2VjcmV0S2V5IjoiQzNqdFNqRnpaSEdBdldORjlzU0syTVdUajF0QktUUVRrTStnR3BCRXBmVT0iLCJwcm9jZXNzZWRIaXN0b3J5TWVzc2FnZXMiOltdLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTWp2a00wRUVJVE9oNlFHR0FFPSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJ6QzljN3g1NGJaV25CVHYvQzUyYmxySzRpRm1wVDNLcVhwcERoRllkb2xJPSIsImFjY291bnRTaWduYXR1cmUiOiIybThDV1BxcjNLSVhlMVJSbVZ1N1dlMDlBZFJyUXovdXNuTTBldUJzTU9SR1JyZy82MFluY24xTmNvbGVuK1E4Y2FZSlFVRHV4d004S3JuU2kySEdEQT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiK0tkWGRVcmNjK2xqbGdmM3E5bkdhZGI5YXZhQlN1NUtUZElsbXRtcCtrYmhqa2NIZnBiNms5N2R4ZnJwYk53Qjhkc3pjSjRUbEhTZ01SUDdjRE1YaHc9PSJ9LCJtZSI6eyJpZCI6Ijk0Nzg1OTE4Njc0OjIwQHMud2hhdHNhcHAubmV0IiwibmFtZSI6Is6x0LrOueKEk86xINGVzrHRj86xzrdnzrEifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTQ3ODU5MTg2NzQ6MjBAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCY3d2WE84ZWVHMlZwd1U3L3d1ZG01YXl1SWhacVU5eXFsNmFRNFJXSGFKUyJ9fV0sInBsYXRmb3JtIjoic21iYSIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTY4NjIzNDg4N30=',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false',
   packname: process.env.PACK_INFO.split(";")[1] || 'Name',
   autoreaction: process.env.AUTO_REACTION || 'off',
   antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
   alwaysonline: process.env.ALWAYS_ONLINE || 'false',
-  antifake : process.env.FAKE_COUNTRY_CODE || '',
+  antifake : process.env.FAKE_COUNTRY_CODE || '92',
   readmessage: process.env.READ_MESSAGE || false,
   HANDLERS: process.env.PREFIX || ['.'],
   warncount : process.env.WARN_COUNT || 3,
